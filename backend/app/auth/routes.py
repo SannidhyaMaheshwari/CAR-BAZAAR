17744e61cc85c95841711f84aacf19b849a44f99
Sannidhya Maheshwari <sannidhyamaheshwari1328@gmail.com>
2025-12-28 00:01:48 +0530

    jwt and json added

--- backend/app/auth/routes.py
+++ backend/app/auth/routes.py
@@ -3,6 +3,8 @@ from bson import ObjectId
 from app.core.database import get_db
 from app.users.models import UserCreate
 from app.auth.services import create_user, authenticate_user
+from app.auth.schemas import LoginRequest
+from app.core.jwt import create_access_token
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
@@ -22,22 +24,32 @@ def register_user(user: UserCreate):
     return {"message": "User registered successfully", "user_id": user_id}
 
 
+
+
 @router.post("/login")
-def login_user(email: str, password: str):
+def login_user(data: LoginRequest):
     db = get_db()
     users = db["users"]
 
-    user = authenticate_user(users, email, password)
+    user = authenticate_user(users, data.email, data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Invalid email or password"
         )
 
-    return {
-        "id": str(user["_id"]),
-        "name": user["name"],
-        "email": user["email"],
+    token = create_access_token({
+        "user_id": str(user["_id"]),
         "user_type": user["user_type"]
+    })
 
+    return {
+        "access_token": token,
+        "token_type": "bearer",
+        "user": {
+            "id": str(user["_id"]),
+            "name": user["name"],
+            "email": user["email"],
+            "user_type": user["user_type"]
+        }
     }
